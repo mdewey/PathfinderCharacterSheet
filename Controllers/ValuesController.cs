eeaba062f8ca3274a01a684d6df3f2f8166af115
Mark Dewey <mark.t.dewey@gmail.com>
2017-07-22 13:41:42 -0400

    first dupm

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactAndDotNet.Models;
+
+namespace ReactAndDotNet.Controllers
+{
+    [Route("api/highscores")]
+    public class ValuesController : Controller
+    {
+       
+    }
+}
